d568fe1c7806910f9286f405d8620ef9c499b3b5
Nazar <nazaranddobriy@gmail.com>
2024-04-08 17:50:50 +0300

    feat: add apiHost and apiKey in env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiHost: 'http://dataservice.accuweather.com',
+  apiKey: 'gIFVdpJpFM2GPeClGcEvPs9G2W3WqnFR'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiHost: 'http://dataservice.accuweather.com',
+  apiKey: 'gIFVdpJpFM2GPeClGcEvPs9G2W3WqnFR'
 };
 
 /*
