f1daa2d1a452345b3e8485691d72d321918bfd50
Nazar <nazaranddobriy@gmail.com>
2024-04-09 23:02:58 +0300

    fix: add weather state and reducer to main

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -2,7 +2,9 @@ import { ActionReducerMap } from "@ngrx/store";
 
 import { IAppState } from "./state";
 import { locationsReducer } from "src/pages/home/store/locations/reducer";
+import { weatherReducer } from "src/pages/home/store/weather/reducer";
 
 export const reducers: ActionReducerMap<IAppState> = {
-  locations: locationsReducer
+  locations: locationsReducer,
+  weather: weatherReducer
 };
--- src/app/store/state.ts
+++ src/app/store/state.ts
@@ -1,5 +1,7 @@
 import { ILocationsState } from "src/pages/home/store/locations/state";
+import { IWeatherState } from "src/pages/home/store/weather/state";
 
 export interface IAppState {
   locations: ILocationsState;
+  weather: IWeatherState;
 }
