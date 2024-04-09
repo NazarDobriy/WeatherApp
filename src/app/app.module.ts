fc8bfc29b8fd7e068a7d685dd2819523c599fd34
Nazar <nazaranddobriy@gmail.com>
2024-04-09 16:56:02 +0300

    fix: update app imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from 'src/core/core.module';
+import { reducers } from './store/reducer';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,15 +17,15 @@ import { CoreModule } from 'src/core/core.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot({}, {}),
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: !isDevMode(),
       autoPause: true
     }),
-    EffectsModule.forRoot([]),
-    HttpClientModule,
-    CoreModule
+    EffectsModule.forRoot(),
+    CoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
