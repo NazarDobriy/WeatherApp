5c7de2216af954672c4440398a94b13608eac886
Nazar <nazaranddobriy@gmail.com>
2024-04-10 19:35:32 +0300

    fix: refactor home component

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -3,23 +3,39 @@ import { Subject, combineLatest, map, takeUntil } from 'rxjs';
 
 import { WeatherStoreService } from './providers/weather-store.service';
 import { IWeather } from './types/weather.interface';
+import { IForecast } from './types/forecast.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  forecasts: IForecast[] = [];
   weather: IWeather | null = null;
+  isLineChart = false;
   isLoading$ = combineLatest([
     this.weatherStore.isLoadingWeather$,
     this.weatherStore.isLoadingForecasts$
   ]).pipe(map((item) => item.some((value) => value)));
   private destroy$ = new Subject<void>();
 
+  get dayDataset(): number[] {
+    return this.forecasts.map((forecast) =>
+      parseFloat(forecast.Temperature.Maximum.Value)
+    );
+  }
+
+  get nightDataset(): number[] {
+    return this.forecasts.map((forecast) =>
+      parseFloat(forecast.Temperature.Minimum.Value)
+    );
+  }
+
   constructor(private weatherStore: WeatherStoreService) {}
 
   ngOnInit(): void {
     this.handleWeather();
+    this.handleForecasts();
   }
 
   private handleWeather(): void {
@@ -27,6 +43,14 @@ export class HomeComponent implements OnInit, OnDestroy {
       .pipe(takeUntil(this.destroy$))
       .subscribe((weather) => {
         this.weather = weather;
+      });
+  }
+
+  private handleForecasts(): void {
+    this.weatherStore.forecasts$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((forecasts) => {
+        this.forecasts = forecasts;
       });
   }
 
