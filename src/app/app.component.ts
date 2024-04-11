677c30f9d9a52d27b5cc9d204deb358b90dcbfa0
Nazar <nazaranddobriy@gmail.com>
2024-04-11 20:25:43 +0300

    fix: update app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { KyivGeoLocation } from 'src/core/consts/location.const';
 import { ErrorHandlerService } from 'src/core/providers/error-handler.service';
 import { LocationStoreService } from 'src/core/providers/location-store.service';
+import { NgRxLocalStorageService } from 'src/core/providers/ng-rx-local-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,19 @@ import { LocationStoreService } from 'src/core/providers/location-store.service'
 export class AppComponent implements OnInit {
   constructor(
     private locationStore: LocationStoreService,
-    private errorHandlerService: ErrorHandlerService
+    private errorHandlerService: ErrorHandlerService,
+    private ngRxLocalStorage: NgRxLocalStorageService
   ) {}
 
   ngOnInit(): void {
     this.handleGeoPosition();
+    this.ngRxLocalStorage.launch();
     this.errorHandlerService.handleError$(this.locationStore.locationFailure$);
   }
 
   private handleGeoPosition(): void {
     if (navigator.geolocation) {
-      return navigator.geolocation.getCurrentPosition(
+      navigator.geolocation.getCurrentPosition(
         (position) => this.locationStore.dispatchLocation(position.coords),
         (error) => {
           this.errorHandlerService.handleError(error.message);
