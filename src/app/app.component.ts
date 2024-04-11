8089d1e483659305925e3ede23ee3af857347684
Nazar <nazaranddobriy@gmail.com>
2024-04-11 15:15:41 +0300

    feat: implement getting geo location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { KyivGeoLocation } from 'src/core/consts/location.const';
+import { ErrorHandlerService } from 'src/core/providers/error-handler.service';
+import { LocationStoreService } from 'src/core/providers/location-store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private locationStore: LocationStoreService,
+    private errorHandlerService: ErrorHandlerService
+  ) {}
+
+  ngOnInit(): void {
+    this.handleGeoPosition();
+    this.errorHandlerService.handleError$(this.locationStore.locationFailure$);
+  }
+
+  private handleGeoPosition(): void {
+    if (navigator.geolocation) {
+      return navigator.geolocation.getCurrentPosition(
+        (position) => this.locationStore.dispatchLocation(position.coords),
+        (error) => {
+          this.errorHandlerService.handleError(error.message);
+          this.locationStore.dispatchLocation(KyivGeoLocation);
+        }
+      );
+    }
+  }
 }
