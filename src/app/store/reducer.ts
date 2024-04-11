8265faa1800de3016350a0b29c1fa6577c44318c
Nazar <nazaranddobriy@gmail.com>
2024-04-11 07:11:05 +0300

    fix: add favorite and location state to app state

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -3,8 +3,10 @@ import { ActionReducerMap } from "@ngrx/store";
 import { IAppState } from "./state";
 import { locationsReducer } from "src/pages/home/store/locations/reducer";
 import { weatherReducer } from "src/pages/home/store/weather/reducer";
+import { favoritesReducer } from "src/core/store/favorites/reducer";
 
 export const reducers: ActionReducerMap<IAppState> = {
   locations: locationsReducer,
-  weather: weatherReducer
+  weather: weatherReducer,
+  favorites: favoritesReducer
 };
--- src/app/store/state.ts
+++ src/app/store/state.ts
@@ -1,7 +1,9 @@
+import { IFavoritesState } from "src/core/store/favorites/state";
 import { ILocationsState } from "src/pages/home/store/locations/state";
 import { IWeatherState } from "src/pages/home/store/weather/state";
 
 export interface IAppState {
   locations: ILocationsState;
   weather: IWeatherState;
+  favorites: IFavoritesState;
 }
