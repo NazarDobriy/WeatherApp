af6c60db3fffe7c352de0f5bdefeab908a20242d
Nazar <nazaranddobriy@gmail.com>
2024-04-11 15:13:24 +0300

    fix: add location reducer and state to global

--- src/app/store/reducer.ts
+++ src/app/store/reducer.ts
@@ -4,8 +4,10 @@ import { IAppState } from "./state";
 import { locationsReducer } from "src/pages/home/store/locations/reducer";
 import { weatherReducer } from "src/pages/home/store/weather/reducer";
 import { favoritesReducer } from "src/core/store/favorites/reducer";
+import { locationReducer } from "src/core/store/location/reducer";
 
 export const reducers: ActionReducerMap<IAppState> = {
+  location: locationReducer,
   locations: locationsReducer,
   weather: weatherReducer,
   favorites: favoritesReducer
