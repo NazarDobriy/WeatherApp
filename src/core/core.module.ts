67581fa1a558cd3fe1bf091b70a90ec3c97b762e
Nazar <nazaranddobriy@gmail.com>
2024-04-11 15:13:58 +0300

    fix: update core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ApiInterceptor } from './interceptors/api.interceptor';
@@ -9,16 +10,23 @@ import { FavoritesStoreService } from './providers/favorites-store.service';
 import { LocationService } from './providers/location.service';
 import { LocationStoreService } from './providers/location-store.service';
 import { SnackBarService } from './providers/snack-bar.service';
+import { LocationEffects } from './store/location/effects';
+import { ErrorHandlerService } from './providers/error-handler.service';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    EffectsModule.forFeature([LocationEffects])
+  ],
   exports: [HeaderComponent],
   providers: [
     FavoritesStoreService,
     LocationService,
     LocationStoreService,
     SnackBarService,
+    ErrorHandlerService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ApiInterceptor,
