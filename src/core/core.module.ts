b911819e3057e97b74295f3277aaed774b1b8151
Nazar <nazaranddobriy@gmail.com>
2024-04-11 07:16:10 +0300

    fix: update imports core

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -5,12 +5,18 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ApiInterceptor } from './interceptors/api.interceptor';
+import { FavoritesStoreService } from './providers/favorites-store.service';
+import { LocationService } from './providers/location.service';
+import { LocationStoreService } from './providers/location-store.service';
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [CommonModule, RouterModule],
   exports: [HeaderComponent],
   providers: [
+    FavoritesStoreService,
+    LocationService,
+    LocationStoreService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ApiInterceptor,
