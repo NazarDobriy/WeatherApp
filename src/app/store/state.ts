6d5e628219b576ad268389f0952b34da238e2a92
Nazar <nazaranddobriy@gmail.com>
2024-04-12 13:39:47 +0300

    fix: connect theme state and reducer to app

--- src/app/store/state.ts
+++ src/app/store/state.ts
@@ -1,5 +1,6 @@
 import { IFavoritesState } from "src/core/store/favorites/state";
 import { ILocationState } from "src/core/store/location/state";
+import { IThemeState } from "src/core/store/theme/state";
 import { ILocationsState } from "src/pages/home/store/locations/state";
 import { IWeatherState } from "src/pages/home/store/weather/state";
 
@@ -8,4 +9,5 @@ export interface IAppState {
   locations: ILocationsState;
   weather: IWeatherState;
   favorites: IFavoritesState;
+  theme: IThemeState;
 }
