604d55606a97cfcdaae64e02e8da8b8ec9de3bb7
nazarii_dobrii <n.dobrii@epom.com>
2025-09-27 11:19:00 +0300

    feat: add notifications

--- src/core/store/favorites/effects.ts
+++ src/core/store/favorites/effects.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 
 import * as FavoritesActions from '@core/store/favorites/actions';
 import { FavoritesService } from '@core/providers/favorites.service';
 import { IFavoriteDetailedInfo } from '@core/types/favorite.interface';
+import { SnackBarService } from '@core/providers/snack-bar.service';
+import { Notification } from '@core/constants/notification.constants';
 
 @Injectable()
 export class FavoritesEffects {
@@ -24,8 +26,30 @@ export class FavoritesEffects {
     );
   });
 
+  failureDetailedFavorites$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(FavoritesActions.getDetailedFavoritesFailure),
+      tap(() => this.snackBarService.open(Notification.ERROR_GETTING_FAVOURITES, 'X')),
+    );
+  }, { dispatch: false });
+
+  successAddShortFavorites$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(FavoritesActions.addShortFavorite),
+      tap(() => this.snackBarService.open(Notification.SUCCESS_ADDING_FAVOURITE,'X')),
+    );
+  }, { dispatch: false });
+
+  successRemoveShortFavorites$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(FavoritesActions.removeShortFavorite),
+      tap(() => this.snackBarService.open(Notification.SUCCESS_REMOVING_FAVOURITE,'X')),
+    );
+  }, { dispatch: false });
+
   constructor(
     private actions$: Actions,
+    private snackBarService: SnackBarService,
     private favoritesService: FavoritesService,
   ) {}
 }
--- src/pages/home/store/weather/effects.ts
+++ src/pages/home/store/weather/effects.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap, tap } from 'rxjs';
 
 import * as WeatherActions from './actions';
 import { WeatherService } from '@core/providers/weather.service';
 import { IWeather } from '@core/types/weather.interface';
 import { IForecast } from '@core/types/forecast.interface';
+import { SnackBarService } from '@core/providers/snack-bar.service';
+import { Notification } from '@core/constants/notification.constants';
 
 @Injectable()
 export class WeatherEffects {
@@ -25,6 +27,13 @@ export class WeatherEffects {
     );
   });
 
+  failureGetWeather$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(WeatherActions.getWeatherFailure),
+      tap(() => this.snackBarService.open(Notification.ERROR_GETTING_WEATHER,'X')),
+    );
+  }, { dispatch: false });
+
   getForecasts$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(WeatherActions.getForecasts),
@@ -41,8 +50,16 @@ export class WeatherEffects {
     );
   });
 
+  failureGetForecasts$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(WeatherActions.getForecastsFailure),
+      tap(() => this.snackBarService.open(Notification.ERROR_GETTING_FORECAST,'X')),
+    );
+  }, { dispatch: false });
+
   constructor(
     private actions$: Actions,
-    private weatherService: WeatherService
+    private weatherService: WeatherService,
+    private snackBarService: SnackBarService,
   ) {}
 }
