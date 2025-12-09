3f4ff1057f5287ea95c4f5f5a8d28474721000ee
Nazar <nazaranddobriy@gmail.com>
2025-12-09 20:53:44 +0200

    fix: add WeatherIcon prop to IWeather interface

--- src/core/types/weather.interface.ts
+++ src/core/types/weather.interface.ts
@@ -1,6 +1,7 @@
 import { ITemperature } from '@core/types/temperature.interface';
 
 export interface IWeather {
+  WeatherIcon: number;
   WeatherText: string;
   Temperature: {
     Metric: ITemperature;
