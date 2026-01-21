40661d04889093ff890d93b2ebf0b4068d7cb024
Nazar <nazaranddobriy@gmail.com>
2026-01-21 19:07:35 +0200

    fix: loader and theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, Inject, OnInit, Renderer2 } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DOCUMENT } from '@angular/common';
+import { AsyncPipe } from "@angular/common";
 
 import { NgRxLocalStorageService } from '@core/providers/ng-rx-local-storage.service';
 import { ThemeStoreService } from '@core/providers/theme-store.service';
@@ -10,38 +9,20 @@ import { HeaderComponent } from '@core/components/header/header.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, HeaderComponent],
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterOutlet, HeaderComponent, AsyncPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
+  readonly isDarkMode$ = this.themeStore.isDarkMode$;
 
   constructor(
-    private renderer: Renderer2,
-    private destroyRef: DestroyRef,
     private themeStore: ThemeStoreService,
     private ngRxLocalStorage: NgRxLocalStorageService,
-    @Inject(DOCUMENT) private document: Document,
   ) {}
 
   ngOnInit(): void {
     this.ngRxLocalStorage.initialization();
-    this.handleBodyBackground();
-  }
-
-  private handleBodyBackground(): void {
-    this.themeStore.isDarkMode$.pipe(
-      takeUntilDestroyed(this.destroyRef),
-    ).subscribe({
-      next: (isDark: boolean) => {
-        const className = 'bg-black-30';
-
-        if (isDark) {
-          this.renderer.addClass(this.document.body, className);
-        } else {
-          this.renderer.removeClass(this.document.body, className);
-        }
-      },
-    });
   }
 
 }
