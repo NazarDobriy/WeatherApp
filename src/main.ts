8acd96aa9bfcb1ca2f43ac461e634247564e8d57
Nazar <nazaranddobriy@gmail.com>
2026-01-22 23:02:19 +0200

    fix: provide zone real detection

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { importProvidersFrom, isDevMode, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { importProvidersFrom, isDevMode, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
@@ -27,7 +27,7 @@ import { WeatherService } from '@core/providers/weather.service';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(appRoutes),
     provideStore(reducers),
     provideEffects(LocationEffects, FavoritesEffects),
