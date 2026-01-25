ea7fe44e83346d14462dac97c2a404b0222fe406
Nazar <nazaranddobriy@gmail.com>
2026-01-25 21:49:48 +0200

    fix: add notifications

--- src/core/constants/notification.constants.ts
+++ src/core/constants/notification.constants.ts
@@ -4,6 +4,8 @@ export const NOTIFICATION = {
   ERROR_GETTING_WEATHER: 'Http failure response for getting weather',
   ERROR_GETTING_FORECAST: 'Http failure response for getting forecast',
   ERROR_GETTING_LOCATION: 'Http failure response for getting location',
+  ERROR_UPDATING_WEATHER: (name: string): string => `Http failure response for updating weather in ${name}`,
   SUCCESS_ADDING_FAVOURITE: 'Success adding location to favorite list',
   SUCCESS_REMOVING_FAVOURITE: 'Success removing location from favorite list',
+  SUCCESS_UPDATING_WEATHER: (name: string): string => `The weather in ${name} has been successfully updated`,
 };
