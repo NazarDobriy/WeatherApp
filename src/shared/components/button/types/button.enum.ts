d85646bf5420880e9a750066d04e9ae2b89e88b4
Nazar <nazaranddobriy@gmail.com>
2026-02-06 14:06:31 +0200

    fix: modify button component

--- src/shared/components/button/types/button.enum.ts
+++ src/shared/components/button/types/button.enum.ts
@@ -2,9 +2,11 @@ export enum ButtonVariant {
   PRIMARY = 'primary',
   UPDATING = 'updating',
   ERROR = 'error',
+  PRIMARY_OUTLINED = 'primary_outlined'
 }
 
 export enum ButtonWidth {
+  XS = 'xs',
   SM = 'sm',
   MD = 'md',
   LG = 'lg',
