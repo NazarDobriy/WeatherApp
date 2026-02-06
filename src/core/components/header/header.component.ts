c7d81dea5ecbd231a9063d761fd10b020be7ff0a
Nazar <nazaranddobriy@gmail.com>
2026-02-06 14:09:15 +0200

    feat: add dialog to the header

--- src/core/components/header/header.component.ts
+++ src/core/components/header/header.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { RouterModule } from '@angular/router';
 import { MatIcon } from "@angular/material/icon";
 import { AsyncPipe } from "@angular/common";
+import { filter } from "rxjs";
 
 import {
   ThemeTemperatureComponent
@@ -11,6 +13,11 @@ import { ButtonComponent } from "@shared/components/button/button.component";
 import { ButtonVariant } from "@shared/components/button/types/button.enum";
 import { FavoritesStoreService } from "@core/providers/favorites-store.service";
 import { REFRESH_KEY } from "@core/constants/loading.constants";
+import { DialogService } from "@core/providers/dialog.service";
+import {
+  RemoveFavoritesDialogComponent,
+} from "@core/components/header/components/dialogs/remove-favorites-dialog/remove-favorites-dialog.component";
+import { IFavoriteDetailedInfo } from "@core/types/favorite.interface";
 
 @Component({
   selector: 'app-header',
@@ -26,19 +33,45 @@ import { REFRESH_KEY } from "@core/constants/loading.constants";
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   readonly buttonVariant = ButtonVariant;
-  readonly detailedFavorites$ = this.favoritesStore.detailedFavorites$;
+  readonly detailedFavorites = signal<IFavoriteDetailedInfo[]>([]);
   readonly selectIsLoadingDetailedFavorites$ = this.favoritesStore.getLoadingSelectByKey(REFRESH_KEY);
 
-  constructor(private favoritesStore: FavoritesStoreService) {}
+  constructor(
+    private destroyRef: DestroyRef,
+    private dialogService: DialogService,
+    private favoritesStore: FavoritesStoreService,
+  ) {}
+
+  ngOnInit(): void {
+    this.favoritesStore.detailedFavorites$.pipe(
+      takeUntilDestroyed(this.destroyRef),
+    ).subscribe({
+      next: (detailedFavorites: IFavoriteDetailedInfo[]) => {
+        this.detailedFavorites.set(detailedFavorites);
+      },
+    });
+  }
 
   refreshAll(): void {
     this.favoritesStore.dispatchDetailedFavorites(REFRESH_KEY);
   }
 
   removeAll(): void {
-    this.favoritesStore.dispatchRemoveFavorites();
+    this.dialogService.open<
+      RemoveFavoritesDialogComponent,
+      { amount: number },
+      boolean
+    >(RemoveFavoritesDialogComponent, {
+      data: { amount: this.detailedFavorites().length },
+      panelClass: 'custom-dialog',
+    }).afterClosed().pipe(
+      filter((item: boolean | undefined) => !!item),
+      takeUntilDestroyed(this.destroyRef),
+    ).subscribe({
+      next: () => this.favoritesStore.dispatchRemoveFavorites(),
+    });
   }
 
 }
