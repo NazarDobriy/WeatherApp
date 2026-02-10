e9652bfd712a1674a1f232d4c6e38b2ba44e6b1e
Nazar <nazaranddobriy@gmail.com>
2026-02-10 19:08:41 +0200

    fix: implement phase component to 5 days forecast

--- src/pages/home/components/forecast-card/forecast-card.component.ts
+++ src/pages/home/components/forecast-card/forecast-card.component.ts
@@ -1,16 +1,26 @@
 import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { MatTooltip } from "@angular/material/tooltip";
 
 import { IForecast } from '@core/types/forecast.interface';
 import { CardComponent } from '@shared/components/card/card.component';
 import { TemperatureConverterPipe } from '@shared/pipes/temperature-converter.pipe';
 import { ForecastCardService } from '@pages/home/components/forecast-card/providers/forecast-card.service';
 import { TemperatureUnit } from '@shared/helpers/temperature-unit.helper';
+import {
+  ForecastPhaseComponent
+} from "@pages/home/components/forecast-card/components/forecast-phase/forecast-phase.component";
 
 @Component({
   selector: 'app-forecast-card',
   templateUrl: './forecast-card.component.html',
-  imports: [DatePipe, CardComponent, TemperatureConverterPipe],
+  imports: [
+    DatePipe,
+    MatTooltip,
+    CardComponent,
+    TemperatureConverterPipe,
+    ForecastPhaseComponent,
+  ],
   providers: [ForecastCardService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
