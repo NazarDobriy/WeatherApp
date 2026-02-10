43304efce0b1b732c9213b56507648ccbb92933c
Nazar <nazaranddobriy@gmail.com>
2026-02-10 19:08:14 +0200

    fix: add ForecastPhase components

---
+++ src/pages/home/components/forecast-card/components/forecast-phase/forecast-phase.component.ts
@@ -0,0 +1,18 @@
+import { Attribute, Component, input } from '@angular/core';
+import { MatTooltip } from "@angular/material/tooltip";
+import { NgOptimizedImage } from "@angular/common";
+
+import { IForecastPhase } from "@core/types/forecast.interface";
+
+@Component({
+  selector: 'app-forecast-phase',
+  imports: [NgOptimizedImage, MatTooltip],
+  templateUrl: './forecast-phase.component.html',
+  styleUrl: './forecast-phase.component.scss',
+})
+export class ForecastPhaseComponent {
+  readonly forecastPhase = input.required<IForecastPhase>();
+
+  constructor(@Attribute('phase') public phase: string) {}
+
+}
