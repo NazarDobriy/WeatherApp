4cd3f708ee0f263cc68ab8cdab1261c7b275a457
Nazar <nazaranddobriy@gmail.com>
2026-02-17 23:19:58 +0200

    fix: storage bugs

--- src/core/constants/storage.constants.ts
+++ src/core/constants/storage.constants.ts
@@ -5,5 +5,3 @@ export const FAVORITES_KEY = 'weather_favorites';
 export const TEMPERATURE_KEY = 'weather_temperature';
 
 export const DAILY_REPRESENTATION_KEY = 'weather_daily_representation';
-
-export const STORAGE_KEYS = [THEME_KEY, FAVORITES_KEY, TEMPERATURE_KEY, DAILY_REPRESENTATION_KEY];
