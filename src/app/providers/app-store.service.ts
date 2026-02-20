540c0733b6ae9db6d813185f07edba8f2575d120
Nazar <nazaranddobriy@gmail.com>
2026-02-20 14:58:36 +0200

    fix: add method for checking route

--- src/app/providers/app-store.service.ts
+++ src/app/providers/app-store.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Store } from "@ngrx/store";
+import { filter, map, Observable } from "rxjs";
 
 import { IAppState } from "@app/store/state";
 import { selectUrl } from "@app/store/selectors";
@@ -9,5 +10,12 @@ export class AppStoreService {
   readonly selectUrl$ = this.store.select(selectUrl);
 
   constructor(private store: Store<IAppState>) {}
+
+  isRoute(path: string): Observable<boolean> {
+    return this.selectUrl$.pipe(
+      filter((url: string | undefined): url is string => typeof url === "string"),
+      map((url: string) => url.startsWith(path)),
+    );
+  }
 
 }
