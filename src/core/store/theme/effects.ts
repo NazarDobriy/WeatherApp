ab28613e19b4092bd99ff2887002172ca9a4a718
Nazar <nazaranddobriy@gmail.com>
2026-02-20 14:59:10 +0200

    fix: do not repeat actions

--- src/core/store/theme/effects.ts
+++ src/core/store/theme/effects.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { tap, withLatestFrom } from "rxjs";
+import { distinctUntilChanged, map, tap, withLatestFrom } from "rxjs";
 
 import * as ThemeActions from './actions';
+import { ThemeType } from "@core/types/theme.type";
 import { ThemeStoreService } from "@core/providers/theme-store.service";
 import { LocalStorageService } from "@core/providers/local-storage.service";
 import { TEMPERATURE_KEY, THEME_KEY, DAILY_REPRESENTATION_KEY } from "@core/constants/storage.constants";
@@ -12,19 +13,18 @@ export class ThemeEffects {
   themeModeStorage$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(ThemeActions.setThemeMode),
-      tap(({ theme }) => {
-        this.localStorageService.set(THEME_KEY, theme);
-      })
+      map(({ theme }) => theme),
+      distinctUntilChanged(),
+      tap((theme: ThemeType) => this.localStorageService.set(THEME_KEY, theme))
     );
   }, { dispatch: false });
 
   themeTemperatureStorage$ = createEffect(() => {
     return this.actions$.pipe(
-      ofType(ThemeActions.toggleTemperature),
-      withLatestFrom(this.themeStore.isCelsius$),
-      tap(([, isCelsius]) => {
-        this.localStorageService.set(TEMPERATURE_KEY, isCelsius);
-      }),
+      ofType(ThemeActions.setTemperature),
+      map(({ isCelsius }) => isCelsius),
+      distinctUntilChanged(),
+      tap((isCelsius: boolean) => this.localStorageService.set(TEMPERATURE_KEY, isCelsius)),
     );
   }, { dispatch: false });
 
@@ -32,9 +32,8 @@ export class ThemeEffects {
     return this.actions$.pipe(
       ofType(ThemeActions.toggleDailyRepresentation),
       withLatestFrom(this.themeStore.isChartRepresentation$),
-      tap(([, isChartRepresentation]) => {
-        this.localStorageService.set(DAILY_REPRESENTATION_KEY, isChartRepresentation);
-      }),
+      map(([, isChartRepresentation]) => isChartRepresentation),
+      tap((isChartRepresentation: boolean) => this.localStorageService.set(DAILY_REPRESENTATION_KEY, isChartRepresentation)),
     );
   }, { dispatch: false });
 
