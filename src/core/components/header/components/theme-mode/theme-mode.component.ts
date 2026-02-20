bb3f1938dbd5f62af7d69ab8d9eeda39745c8745
Nazar <nazaranddobriy@gmail.com>
2026-02-20 15:02:32 +0200

    fix: implement new select in header

--- src/core/components/header/components/theme-mode/theme-mode.component.ts
+++ src/core/components/header/components/theme-mode/theme-mode.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AsyncPipe } from "@angular/common";
 
+import { ThemeType } from "@core/types/theme.type";
 import { ThemeStoreService } from '@core/providers/theme-store.service';
 import { SelectComponent}  from "@shared/components/select/select.component";
 import { THEME_GROUPS } from "@core/components/header/constants/theme-select.constants";
-import { ThemeType } from "@core/types/theme.type";
+import { HeaderFacadeService } from "@core/components/header/providers/header-facade.service";
 
 @Component({
   selector: 'app-theme-mode',
@@ -16,7 +17,10 @@ export class ThemeModeComponent {
   readonly THEME_GROUPS = THEME_GROUPS;
   readonly theme$ = this.themeStore.theme$;
 
-  constructor(private themeStore: ThemeStoreService) {}
+  constructor(
+    public headerFacade: HeaderFacadeService,
+    private themeStore: ThemeStoreService,
+  ) {}
 
   changeThemeMode(theme: ThemeType | null): void {
     if (theme) {
--- src/core/components/header/components/theme-temperature/theme-temperature.component.ts
+++ src/core/components/header/components/theme-temperature/theme-temperature.component.ts
@@ -1,20 +1,31 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 
+import { SelectGap } from "@shared/components/select/types/select.enum";
 import { ThemeStoreService } from '@core/providers/theme-store.service';
+import { SelectComponent } from "@shared/components/select/select.component";
+import { TEMPERATURE_GROUPS } from "@core/components/header/constants/temperature-select.constants";
+import { HeaderFacadeService } from "@core/components/header/providers/header-facade.service";
 
 @Component({
   selector: 'app-theme-temperature',
   templateUrl: './theme-temperature.component.html',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, SelectComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ThemeTemperatureComponent {
+  readonly selectGap = SelectGap;
+  readonly TEMPERATURE_GROUPS = TEMPERATURE_GROUPS;
   readonly isCelsius$ = this.themeStore.isCelsius$;
 
-  constructor(private themeStore: ThemeStoreService) {}
+  constructor(
+    public headerFacade: HeaderFacadeService,
+    private themeStore: ThemeStoreService,
+  ) {}
 
-  changeThemeTemperature(): void {
-    this.themeStore.dispatchTemperature();
+  changeThemeTemperature(isCelsius: boolean | null): void {
+    if (isCelsius !== null) {
+      this.themeStore.dispatchSetTemperature(isCelsius);
+    }
   }
 }
