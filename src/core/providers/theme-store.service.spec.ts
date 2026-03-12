f7c5e5d78f85feba1fd2384663a4768b34f385eb
Nazar <nazaranddobriy@gmail.com>
2026-03-12 22:07:23 +0200

    fix: write tests

--- src/core/providers/theme-store.service.spec.ts
+++ src/core/providers/theme-store.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { provideStore } from '@ngrx/store';
 
 import { ThemeStoreService } from './theme-store.service';
 
@@ -6,7 +8,9 @@ describe('ThemeStoreService', () => {
   let service: ThemeStoreService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideStore({}), provideZonelessChangeDetection()],
+    });
     service = TestBed.inject(ThemeStoreService);
   });
 
