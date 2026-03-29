b7e1b808ab69055dd25664241a73b38e10e4e7ca
Nazar <nazaranddobriy@gmail.com>
2026-03-29 23:34:59 +0300

    fix: remove comments

--- src/core/store/weather/actions.ts
+++ src/core/store/weather/actions.ts
@@ -12,18 +12,6 @@ export const getWeatherSuccess = createAction(
 
 export const getWeatherFailure = createAction('[Weather] Get Weather failure', props<{ error: string }>());
 
-// export const getForecasts = createAction('[Weather] Get Forecasts', props<{ key: string }>());
-//
-// export const getForecastsSuccess = createAction(
-//   '[Weather] Get Forecasts success',
-//   props<{ forecasts: IForecast[] }>(),
-// );
-//
-// export const getForecastsFailure = createAction(
-//   '[Weather] Get Forecasts failure',
-//   props<{ error: string }>(),
-// );
-
 export const updateWeather = createAction('[Weather] Update Weather', props<{ key: string; name: string }>());
 
 export const updateWeatherSuccess = createAction(
