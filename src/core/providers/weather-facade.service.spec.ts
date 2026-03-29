fc4d45cb62e0e63e5a90828d78d1943220082c80
Nazar <nazaranddobriy@gmail.com>
2026-03-29 23:31:30 +0300

    fix: tests for WeatherFacadeService

--- src/core/providers/weather-facade.service.spec.ts
+++ src/core/providers/weather-facade.service.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 import { WeatherFacadeService } from './weather-facade.service';
+import { WeatherService } from '@core/providers/weather.service';
+import { MockWeatherService } from '@tests/providers/mock-weather.service';
 
 describe('WeatherFacadeService', () => {
   let service: WeatherFacadeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        WeatherFacadeService,
+        provideZonelessChangeDetection(),
+        { provide: WeatherService, useClass: MockWeatherService },
+      ],
+    });
     service = TestBed.inject(WeatherFacadeService);
   });
 
